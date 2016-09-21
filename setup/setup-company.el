84f11d6c03fdb30d8d29e0de6c41dca8500141bf
ANDRON94 <makedonsky94@gmail.com>
2016-09-21 17:31:54 +0300

    [Fix]Fix behavior of company mode: - disable downcasing at dabbrev backend; - merge results of capf and dabbrev backends.

--- setup/setup-company.el
+++ setup/setup-company.el
@@ -1,6 +1,10 @@
 (require 'company)
 
 ;; General setup
-(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'global-company-mode) ; Activate company mode globally
+(setq company-dabbrev-downcase nil) ; Disable downcasing of autocompletes with dabbrev
+
+;; Merge results of capf and dabbrev backends
+(setf (car (member 'company-capf company-backends)) '(company-capf company-dabbrev))
 
 (provide 'setup-company)
