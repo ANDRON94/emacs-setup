5a461f65136ffc67f5689af7246c6f3b0fef6c08
ANDRON94 <makedonsky94@gmail.com>
2016-09-21 11:01:23 +0300

    [Fix]Disable 'yasnippet' minor mode in 'term' major mode because it's disable autocompleting with <TAB>.

--- setup/setup-yasnippet.el
+++ setup/setup-yasnippet.el
@@ -1,6 +1,8 @@
 (require 'yasnippet)
 
 ;; General setup
-(yas-global-mode 1)
+(yas-global-mode 1) ; Enable yasnippet globaly
+(add-hook 'term-mode-hook (lambda()
+                (yas-minor-mode -1))) ; Disable yasnippet in terminal mode 
 
 (provide 'setup-yasnippet)
