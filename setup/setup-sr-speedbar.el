d900311144388dc36d99309aa8a896b32123977c
ANDRON94 <makedonsky94@gmail.com>
2016-09-23 17:29:02 +0300

    [Fix]Now speedbar correctly display hidden files.

--- setup/setup-sr-speedbar.el
+++ setup/setup-sr-speedbar.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "<f2>") 'sr-speedbar-toggle)
 
 ;; General setup
+(setq speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.\\.*$\\)\\'")
 (setq speedbar-show-unknown-files t) ; show all files
 ;(setq speedbar-use-images nil) ; use text for buttons
 (setq sr-speedbar-right-side nil) ; put on left side
