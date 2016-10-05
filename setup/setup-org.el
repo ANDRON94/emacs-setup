14ec6a5cbcd6165046c8ffbaa7a65ab21a388276
Andrii Tymchuk <Andrii.Tymchuk@harmonicinc.com>
2016-10-05 19:59:25 +0300

    [Update]Upgrade org mode:
    - add hotkey for capture mode;
    - customize org directory and modify agenda files path structure.

--- setup/setup-org.el
+++ setup/setup-org.el
@@ -1,8 +1,9 @@
 (use-package org
   :ensure t
   :bind (("C-c l" . org-store-link)
-                                        ;("C-c c" . org-capture)
+         ("C-c c" . org-capture)
          ("C-c a" . org-agenda))
   :config
   (setq org-log-done t)
-  (setq org-agenda-files (list "~/.emacs.d/org/holidays.org")))
+  (setq org-directory "~/.emacs.d/org")
+  (setq org-agenda-files (list (concat org-directory "/holidays.org"))))
