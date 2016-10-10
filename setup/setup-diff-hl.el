89b4205777cb21d28b0409fe725f996e6a577840
Andrii Tymchuk <Andrii.Tymchuk@harmonicinc.com>
2016-10-10 20:13:10 +0300

    [Update]Setup hotkeys for diff-hl mode.

--- setup/setup-diff-hl.el
+++ setup/setup-diff-hl.el
@@ -1,7 +1,10 @@
 (use-package diff-hl
+  :ensure t
   :init
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-  :ensure t
+  :bind (("C-c d n" . diff-hl-next-hunk)
+         ("C-c d p" . diff-hl-previous-hunk)
+         ("C-c d r" . diff-hl-revert-hunk))
   :config
   (global-diff-hl-mode +1)
   (diff-hl-flydiff-mode +1))
