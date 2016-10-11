62bd3c9cc2e548350af8d7b43a94ff4de855a218
Andrii Tymchuk <Andrii.Tymchuk@harmonicinc.com>
2016-10-11 11:42:40 +0300

    [Fix]Fix diff-hl mode activation:
    - remove key bindings from ':bind' form;
    - use 'global-set-key' and 'add-hook' functions
    in a ':config' form.

--- setup/setup-diff-hl.el
+++ setup/setup-diff-hl.el
@@ -1,10 +1,9 @@
 (use-package diff-hl
   :ensure t
-  :init
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-  :bind (("C-c d n" . diff-hl-next-hunk)
-         ("C-c d p" . diff-hl-previous-hunk)
-         ("C-c d r" . diff-hl-revert-hunk))
   :config
+  (global-set-key (kbd "C-c d n") 'diff-hl-next-hunk)
+  (global-set-key (kbd "C-c d p") 'diff-hl-previous-hunk)
+  (global-set-key (kbd "C-c d r") 'diff-hl-revert-hunk)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
   (global-diff-hl-mode +1)
   (diff-hl-flydiff-mode +1))
