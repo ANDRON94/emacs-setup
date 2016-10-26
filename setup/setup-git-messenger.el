118276f439c3b5357b3925a08fddd32c64309472
ANDRON94 <makedonsky94@gmail.com>
2016-10-26 23:39:27 +0300

    [Update] Change hotkeys in VCS packages:
    use uniform hotkeys for magit, diff-hl,
    git-messenger cause they all working
    with [V]CS.

--- setup/setup-git-messenger.el
+++ setup/setup-git-messenger.el
@@ -1,6 +1,6 @@
 (use-package git-messenger
   :ensure t
-  :bind (("C-c v p" . git-messenger:popup-message))
+  :bind (("C-c v m" . git-messenger:popup-message))
                                         ;:map git-messenger-map - Default keybindings is pretty good
                                         ;())
   :config)
--- setup/setup-magit.el
+++ setup/setup-magit.el
@@ -3,8 +3,8 @@
   :commands (diff-hl-magit-post-refresh)
   :init
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh) ; Integrate magit with diff-hl
-  :bind (("C-c m s" . magit-status)
-         ("C-c m i" . magit-init)
-         ("C-c m c" . magit-clone))
+  :bind (("C-c v s" . magit-status)
+         ("C-c v i" . magit-init)
+         ("C-c v c" . magit-clone))
   :config
   (use-package magit-svn :ensure t))
