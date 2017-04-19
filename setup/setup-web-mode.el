86690f2b26e5dff3cb700a6fa2328efc716d077b
Andrii Tymchuk <atymchuk@debian.hlit.local>
2017-04-19 19:29:37 +0300

    [Update] Setup indentination for web-mode.

--- setup/setup-web-mode.el
+++ setup/setup-web-mode.el
@@ -5,4 +5,6 @@
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2))
