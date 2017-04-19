1602857e010d31ef645589e7eaaff3dbd14bfc0c
Andrii Tymchuk <atymchuk@debian.hlit.local>
2017-04-19 19:30:13 +0300

    [Codestyle] Move from 'setf' to 'setq'.

--- setup/setup-company.el
+++ setup/setup-company.el
@@ -9,7 +9,7 @@
     :bind (:map company-active-map
            ("M-h" . company-quickhelp-manual-begin))
     :config
-    (setf company-quickhelp-delay nil)
+    (setq company-quickhelp-delay nil)
                                         ; (company-quickhelp-mode +1)
     )
   (use-package company-irony
@@ -20,6 +20,6 @@
     (add-to-list 'company-backends '(company-irony-c-headers company-irony))
     (with-eval-after-load 'irony
       (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)))
-  (setf (car (member 'company-capf company-backends))
+  (setq (car (member 'company-capf company-backends))
         '(company-capf company-dabbrev)) ; Merge results of capf and dabbrev backends
   (setq company-dabbrev-downcase nil)) ; Disable downcasing of autocompletes with dabbrev
