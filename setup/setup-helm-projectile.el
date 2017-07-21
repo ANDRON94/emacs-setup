1ab324113ef623130534ae9a7edf3cd70de1a4fe
Andrii Tymchuk <atymchuk@KV-LT-6230V5K.hlit.local>
2017-07-21 12:22:21 +0300

    [Update] Remove from grep projectile search 'gtags' utility files.

--- setup/setup-helm-projectile.el
+++ setup/setup-helm-projectile.el
@@ -6,4 +6,8 @@
   (setq projectile-completion-system 'helm)
   (helm-projectile-on)
                                         ;(setq projectile-enable-caching t) ; caching
-  (setq projectile-switch-project-action 'helm-projectile)) ;mode of action after switch a project
+  (setq projectile-switch-project-action 'helm-projectile) ;mode of action after switch a project
+  ;; TODO: move custom settings to a separate file!!!
+  (add-to-list 'projectile-globally-ignored-files "GPATH")
+  (add-to-list 'projectile-globally-ignored-files "GTAGS")
+  (add-to-list 'projectile-globally-ignored-files "GRTAGS"))
