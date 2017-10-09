35b3efa373caac6338b362fc67a3ebc4ac3c0fb6
Andrii Tymchuk <atymchuk@KV-LT-6230V5K.hlit.local>
2017-10-09 15:58:07 +0300

    [Update] Remove obsolete configuration files.

--- setup/setup-diff-hl.el
+++
@@ -1,8 +0,0 @@
-(use-package diff-hl
-  :ensure t
-  :config
-  (global-set-key (kbd "C-c d n") 'diff-hl-next-hunk)
-  (global-set-key (kbd "C-c d p") 'diff-hl-previous-hunk)
-  (global-set-key (kbd "C-c d r") 'diff-hl-revert-hunk)
-  (global-diff-hl-mode +1)
-  (diff-hl-flydiff-mode +1))
--- setup/setup-yasnippet.el
+++
@@ -1,6 +0,0 @@
-(use-package yasnippet
-  :ensure t
-  :init
-  (add-hook 'term-mode-hook (lambda() (yas-minor-mode -1))) ; Disable yasnippet in terminal mode
-  :config
-  (yas-global-mode +1)) ; Enable yasnippet globaly
