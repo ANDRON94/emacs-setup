8e5a9e919fad42021e9efbf21430fdc3b9e8e30b
Andrii Tymchuk <atymchuk@KV-LT-6230V5K.hlit.local>
2017-12-11 14:32:11 +0200

    [Update] Add feature list to README file.

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 ;;; Code:
 
 ;; Define configuration version.
-(defconst my-config-version "2.0.4"
+(defconst my-config-version "2.0.5"
   "Version number of this configuration.")
 
 ;; Configure init file and emacs directory path.
