475068880f466b2cb4eb085535021c0cf8a23c99
Andrii Tymchuk <atymchuk@KV-LT-6230V5K.hlit.local>
2018-02-23 15:12:38 +0200

    [Update] Add official 'org' repository to the list of package archives.

--- configuration/loader/my-unconditional-loader.el
+++ configuration/loader/my-unconditional-loader.el
@@ -17,6 +17,7 @@
 ;; Remote package archive setup.
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 ;; (add-to-list 'package-archives
 ;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
 ;; (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
