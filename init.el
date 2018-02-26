0bdc9af67facc772315c742980d6a9ee1ec7ffff
Andrii Tymchuk <atymchuk@KV-LT-6230V5K.hlit.local>
2018-02-26 18:42:25 +0200

    [Update] Describe new packages and features in README.

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 ;(package-initialize)
 
 ;; Define configuration version.
-(defconst my-config-version "2.10.16"
+(defconst my-config-version "2.10.17"
   "Version number of this configuration.")
 
 ;; Configure init file and emacs directory path.
