01b126cb5c340db9a5f6597f9e3be399cd3a2217
Andrii Tymchuk <makedonsky94@gmail.com>
2019-06-24 16:35:25 +0300

    [Update] Set path for 'multi-compile' util files to the common directory.

--- configuration/layers/interface-enchancement/layer-interface-enchancement.el
+++ configuration/layers/interface-enchancement/layer-interface-enchancement.el
@@ -22,6 +22,10 @@
   ;; Make "multi-compile-alist" safe as local variable.
   ;; It's useful for creating ".dir-locals.el".
   (put 'multi-compile-alist 'safe-local-variable #'listp)
+  ;; Move 'multi-compile' related util files to the common directory.
+  ;; TODO: automatically create non-existing directories.
+  (setq multi-compile-history-file (my-emacs-absolute-path
+                                    ".cache/multi-compile/multi-compile.cache"))
   ;; Set "helm" completion system after "helm" is loaded.
   (with-eval-after-load 'helm
     (setq multi-compile-completion-system 'helm)))
