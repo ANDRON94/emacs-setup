57c56edeca9caf51f17aa5f55192c6c0a64d17e9
Andrii Tymchuk <makedonsky94@gmail.com>
2019-06-24 13:19:13 +0300

    [Create] Add new configuration layer - 'interface-enchancement'.

---
+++ configuration/layers/interface-enchancement/layer-interface-enchancement.el
@@ -0,0 +1,19 @@
+;;; layer-interface-enchancement.el --- Interface enchancement(usability, ergonomics) settings.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Andrii Tymchuk
+
+;; Author: Andrii Tymchuk <makedonsky94@gmail.com>
+;; Keywords: lisp
+
+;;; Commentary:
+
+;; load all interface enchancement layers
+
+;;; Code:
+
+;; general
+
+;; Use "y" or "n" instead of "yes" or "not".
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; layer-interface-enchancement.el ends here
